467919810f815af532efe4f5cda45319b113ad54
Dhruvinpatel06 <pateldhruvin2506@gmail.com>
2026-07-30 15:55:38 +0530

    implement modular footer component and initialize header navigation configuration

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,4 @@
 import React, { Suspense } from "react";
-import Link from "next/link";
-import { footer } from "./config";
-import { Button } from "../ui/button";
 import SocialMediaButtons from "../social/social-media-icons";
 import { config } from "@/data/config";
 
@@ -17,21 +14,6 @@ function Footer() {
         © <Suspense fallback={<>2026</>}><CopyrightYear /></Suspense> {config.author}. All rights reserved.
       </p>
       <SocialMediaButtons />
-      <nav className="flex gap-4 sm:gap-6 z-10">
-        {footer.map((link, index) => {
-          const { title, href } = link;
-
-          return (
-            <Link
-              className="text-xs underline-offset-4 hover:underline"
-              href={href}
-              key={`l_${index}`}
-            >
-              <Button variant={"link"}>{title}</Button>
-            </Link>
-          );
-        })}
-      </nav>
     </footer>
   );
 }
